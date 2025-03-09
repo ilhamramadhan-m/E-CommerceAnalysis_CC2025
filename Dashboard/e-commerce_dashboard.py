8a8f0a38c732d1dcd37652e6147f24ad024de13e
Muhammad Ilham Ramadhan <ilhammuhramadhan@gmail.com>
2025-03-09 20:15:29 +0700

    Update e-commerce_dashboard.py

--- Dashboard/e-commerce_dashboard.py
+++ Dashboard/e-commerce_dashboard.py
@@ -9,9 +9,6 @@ df = pd.read_csv("Dashboard/e-commerce_dataset.csv", parse_dates=["order_purchas
 # Set page config
 st.set_page_config(page_title="E-Commerce Analysis Dashboard", layout="wide")
 
-# memberikan logo pada sidebar dan dashboard
-st.sidebar.image("Dashboard/coding_camp_logo.png", use_column_width=True)
-
 # membuat header pada sidebar
 st.sidebar.header("🔍 Filter")
 
